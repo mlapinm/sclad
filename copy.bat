90028f149b6a17d0b842962e227644f75e740c6a
Mikhail Lapin <mlapin@rambler.ru>
2019-04-03 13:37:11 +0300

    new sclad

---
+++ copy.bat
@@ -0,0 +1,1 @@
+copy 01*.txt+02*.txt+04*.txt+30*.txt+31*.txt+32*.txt+33*.txt+34*.txt+35*.txt+36*.txt+40*.txt+42*.txt+44*.txt+46*.txt+50*.txt+52*.txt+80*.txt+81*.txt+82*.txt+83*.txt+84*.txt+86*.txt  all.txt
